f28ffadd3f7917f41f6eb6362de2563817fd3ca7
Jokimax <jonaxthedreadlord@gmail.com>
2024-07-13 22:12:07 +0300

    update

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 My portfolio website for job applications.
-Link to the site can be found here: <a>jokimax.com</a>.
+Link to the site can be found here: <a href="jokimax.com">jokimax.com</a>.
 Feel free to copy the website and use it in your projects.
--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,20 @@
-import {createBrowserRouter, RouterProvider} from "react-router-dom"
-import PageBase from "./pageBase"
-import Home from './homepage'
-import PrivacyPolicy from "./privacyPolicy"
-import Error404 from "./404"
+import { HashRouter, Routes, Route } from "react-router-dom";
+import PageBase from "./pageBase";
+import Home from './homepage';
+import PrivacyPolicy from "./privacyPolicy";
+import Error404 from "./404";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      element: <PageBase />,
-      children: [
-        {
-          path: "/",
-          element: <Home />,
-        },
-        {
-          path: "/simplydodge/privacyPolicy",
-          element: <PrivacyPolicy />,
-        },
-        {
-          path: "*",
-          element: <Error404 />,
-        },
-      ],
-    },
-  ])
   return (
-    <RouterProvider router={router} />
-  )
+    <HashRouter>
+      <PageBase />
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/simplydodge/privacypolicy" Component={PrivacyPolicy} />
+        <Route path="*" Component={Error404} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
-export default App
+export default App;
